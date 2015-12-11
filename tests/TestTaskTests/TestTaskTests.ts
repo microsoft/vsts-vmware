cf9bd403766226a3c0df6fe4495bcc53392db56c
Arun Mahapatra <armahapa@microsoft.com>
2015-12-11 18:46:23 +0530

    Add support for code coverage with istanbul.

--- tests/TestTaskTests/TestTaskTests.ts
+++ tests/TestTaskTests/TestTaskTests.ts
@@ -6,11 +6,9 @@ import * as testTask from "../../src/Tasks/TestTask/testTask";
 describe("HelloWorldWriter.Write", () : void => {
   it("should throw not implemented error", () : void => {
       assert.doesNotThrow(() : void => {
-      // dummy code
-      var helloWorldWriter = new testTask.HelloWorldWriter();
-      helloWorldWriter.Write();
+        // dummy code
+        var helloWorldWriter = new testTask.HelloWorldWriter();
+        helloWorldWriter.Write();
     });
   });
 });
-
-
