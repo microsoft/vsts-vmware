e64ad1c5b835f5f543ac3b3e2a5a4a0240c741b5
Subrahmanyam Mandavilli <subraman@microsoft.com>
2015-12-31 17:21:36 +0530

    Made VM name case in sensitive

--- tests/Tools/vmOpsTool/InMemoryVMWareImpl.java
+++ tests/Tools/vmOpsTool/InMemoryVMWareImpl.java
@@ -5,17 +5,17 @@ public class InMemoryVMWareImpl implements IVMWare {
 
     private Map<String, Map<String, Integer>> vmSnapshotInfo = new HashMap<String, Map<String, Integer>>();
     private Map<String, Integer> snapshotMap = new HashMap<String, Integer>();
-    
+
     public InMemoryVMWareImpl() {
         snapshotMap.put("Snapshot1", 0);
         snapshotMap.put("Snapshot2", 1);
-        vmSnapshotInfo.put("TestVM1", snapshotMap);
-        vmSnapshotInfo.put("PoweredOffVM", snapshotMap);
-        vmSnapshotInfo.put("DuplicateVMName", snapshotMap);
-        vmSnapshotInfo.put("TemplateVM", snapshotMap);
-        vmSnapshotInfo.put("VMTemplate", snapshotMap);
-        vmSnapshotInfo.put("VMInDC1", snapshotMap);
-        vmSnapshotInfo.put("VMInDC2", snapshotMap);
+        vmSnapshotInfo.put("testvm1", snapshotMap);
+        vmSnapshotInfo.put("poweredoffvm", snapshotMap);
+        vmSnapshotInfo.put("duplicatevmname", snapshotMap);
+        vmSnapshotInfo.put("templatevm", snapshotMap);
+        vmSnapshotInfo.put("vmtemplate", snapshotMap);
+        vmSnapshotInfo.put("vmindc1", snapshotMap);
+        vmSnapshotInfo.put("vmindc2", snapshotMap);
         vmSnapshotInfo.put("vm1", snapshotMap);
         vmSnapshotInfo.put("vm2", snapshotMap);
     }
@@ -25,26 +25,24 @@ public class InMemoryVMWareImpl implements IVMWare {
         Map<String, Integer> cpMap = null;
 
         for (String vm : vms) {
-            vm = vm.trim();
-            
-            if(vmSnapshotInfo.containsKey(vm)) {
+            vm = vm.trim().toLowerCase();
+
+            if (vmSnapshotInfo.containsKey(vm)) {
                 cpMap = vmSnapshotInfo.get(vm);
-                if(!cpMap.containsKey(snapshotName)) {
+                if (!cpMap.containsKey(snapshotName)) {
                     System.out.println("Snapshot does not exist: " + snapshotName);
                     throw new Exception("Snapshot does not exist: " + snapshotName);
                 }
-                
+
                 for (Map.Entry<String, Integer> mapEntry : cpMap.entrySet()) {
-                    if(mapEntry.getKey().equalsIgnoreCase(snapshotName)) {
+                    if (mapEntry.getKey().equalsIgnoreCase(snapshotName)) {
                         mapEntry.setValue(1);
                         System.out.println("Restored snapshot " + snapshotName);
-                    }
-                    else {
+                    } else {
                         mapEntry.setValue(0);
                     }
                 }
-            }
-            else {
+            } else {
                 throw new Exception("VM not found.");
             }
         }
@@ -52,7 +50,7 @@ public class InMemoryVMWareImpl implements IVMWare {
     }
 
     public void connect(ConnectionData connData) throws Exception {
-        if(connData.password.equals("InvalidPassword")) {
+        if (connData.password.equals("InvalidPassword")) {
             throw new Exception();
         }
     }
@@ -61,10 +59,11 @@ public class InMemoryVMWareImpl implements IVMWare {
 
         Map<String, Integer> cpMap = null;
         String currentSnapshotName = null;
+        vmName = vmName.toLowerCase();
         cpMap = vmSnapshotInfo.get(vmName);
 
         for (Map.Entry<String, Integer> mapEntry : cpMap.entrySet()) {
-            if(mapEntry.getValue().equals(1)) {
+            if (mapEntry.getValue().equals(1)) {
                 currentSnapshotName = mapEntry.getKey();
             }
         }
--- tests/Tools/vmOpsTool/VMWarePlatformTests.java
+++ tests/Tools/vmOpsTool/VMWarePlatformTests.java
@@ -35,7 +35,7 @@ public abstract class VMWarePlatformTests {
         vmWareImpl.connect(connData);
     }
 
-    // Common for restore/delete snapshot operations 
+    // Common for restore/delete snapshot operations
     @Test
     public void restoreSnapshotShouldThrowIfSnapshotDoesNotExist() {
         Exception exp = null;
@@ -70,14 +70,13 @@ public abstract class VMWarePlatformTests {
 
     @Test
     public void restoreSnapshotShouldRestoreSnapshotForTwoVms() throws Exception {
-        
-        String vmList = "PoweredOffVM, TestVM1";
-        
-        
+
+        String vmList = "PoweredoffVM, testVM1";
+
         vmWareImpl.restoreSnapshot(vmList, snapshotOne, connData);
         assertThat(vmWareImpl.getCurrentSnapshot("PoweredOffVM", connData)).isEqualTo(snapshotOne);
         assertThat(vmWareImpl.getCurrentSnapshot("TestVM1", connData)).isEqualTo(snapshotOne);
-        
+
         vmWareImpl.restoreSnapshot(vmList, snapshotTwo, connData);
         assertThat(vmWareImpl.getCurrentSnapshot("PoweredOffVM", connData)).isEqualTo(snapshotTwo);
         assertThat(vmWareImpl.getCurrentSnapshot("TestVM1", connData)).isEqualTo(snapshotTwo);
@@ -98,32 +97,32 @@ public abstract class VMWarePlatformTests {
         String vmName2 = "VMTemplate";
         vmWareImpl.restoreSnapshot(vmName1, snapshotOne, connData);
         vmWareImpl.restoreSnapshot(vmName2, snapshotOne, connData);
-        
+
         assertThat(vmWareImpl.getCurrentSnapshot(vmName1, connData)).isEqualTo(snapshotOne);
         assertThat(vmWareImpl.getCurrentSnapshot(vmName2, connData)).isEqualTo(snapshotOne);
-        
+
         vmWareImpl.restoreSnapshot(vmName1, snapshotTwo, connData);
         vmWareImpl.restoreSnapshot(vmName2, snapshotTwo, connData);
-        
+
         assertThat(vmWareImpl.getCurrentSnapshot(vmName1, connData)).isEqualTo(snapshotTwo);
         assertThat(vmWareImpl.getCurrentSnapshot(vmName2, connData)).isEqualTo(snapshotTwo);
     }
-    
+
     @Test
     public void restoreSnapshotShouldRestoreSnapshotForVMsDiffDataCenters() throws Exception {
-        
+
         String vmName1 = "VMInDC1";
         String vmName2 = "VMInDC2";
-        
+
         vmWareImpl.restoreSnapshot(vmName1, "Snapshot1", connData);
         vmWareImpl.restoreSnapshot(vmName2, "Snapshot1", connData);
-        
+
         assertThat(vmWareImpl.getCurrentSnapshot(vmName1, connData)).isEqualTo(snapshotOne);
         assertThat(vmWareImpl.getCurrentSnapshot(vmName2, connData)).isEqualTo(snapshotOne);
-        
+
         vmWareImpl.restoreSnapshot(vmName1, "Snapshot2", connData);
         vmWareImpl.restoreSnapshot(vmName2, "Snapshot2", connData);
-        
+
         assertThat(vmWareImpl.getCurrentSnapshot(vmName1, connData)).isEqualTo(snapshotTwo);
         assertThat(vmWareImpl.getCurrentSnapshot(vmName2, connData)).isEqualTo(snapshotTwo);
     }
