a65b65ff3dcf954c1d040b1618cdbb362a18486c
Subrahmanyam Mandavilli <subraman@microsoft.com>
2016-02-11 10:04:50 +0530

    1. Added skipCA check for the task 2. Request timeout and connection timeout

--- src/Tools/vmOpsTool/ConnectionData.java
+++ src/Tools/vmOpsTool/ConnectionData.java
@@ -1,6 +1,9 @@
-
-
 public class ConnectionData {
+
+    private String userName;
+    private String password;
+    private String url;
+    private boolean skipCACheck;
 
     public ConnectionData(String vCenterUrl, String vCenterUserName, String vCenterPassword, boolean skipCACheck) {
         this.userName = vCenterUserName;
@@ -9,8 +12,19 @@ public class ConnectionData {
         this.skipCACheck = skipCACheck;
     }
 
-    public String userName;
-    public String password;
-    public String url;
-    public boolean skipCACheck;
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public boolean isSkipCACheck() {
+        return skipCACheck;
+    }
 }
--- tests/Tools/vmOpsTool/InMemoryVMWareImpl.java
+++ tests/Tools/vmOpsTool/InMemoryVMWareImpl.java
@@ -80,7 +80,7 @@ public class InMemoryVMWareImpl implements IVMWare {
     }
 
     public void connect(ConnectionData connData) throws Exception {
-        if (connData.password.equals("InvalidPassword") || !connData.skipCACheck) {
+        if (connData.getPassword().equals("InvalidPassword") || !connData.isSkipCACheck()) {
             throw new Exception();
         }
     }
